7570434d5eb9322c4a5a95041daeecf929a993b7
Chris Ballinger <cballinger@rightpoint.com>
2019-04-11 10:55:50 -0700

    Initial commit

---
+++ 1.7.0/Regression/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Regression
+//
+//  Created by Chris Ballinger on 4/11/19.
+//  Copyright © 2019 Example. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+
+    let animationView = AnimationView(name: "test")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# CocoaPods 1.7.0 Regression
+
+This is a sample project to demonstrate a regression in CocoaPods 1.7.0 beta.
+
+* https://github.com/CocoaPods/CocoaPods/issues/8695
